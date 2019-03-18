12d6eb424ec201399dab1d16d2e35a0786ae9d58
Daniel <schnolldj@gmail.com>
2019-03-18 17:18:19 -0400

    starting srt

---
+++ SRT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+#include <string>
+#include <deque>
+#include <iomanip>
+#include "process.h"
+
+using namespace std;
+      // process vector | num inputs
+void srt(vector<Process> p, int n){
+
+}
