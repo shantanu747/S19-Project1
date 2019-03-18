819053cc7d5f05e3f5a60289779b41281eb70435
Shantanu Patil <shantanu747@gmail.com>
2019-03-18 17:26:20 -0400

    Update SRT.cpp

--- SRT.cpp
+++ SRT.cpp
@@ -5,14 +5,23 @@
 #include <string>
 #include <deque>
 #include <iomanip>
+#include <algorithm>
 #include "process.h"
 
 using namespace std;
       // process vector | num inputs
+
+bool sortHelper(Process a, Process b)
+{
+  return (a.getRemainingTimeInBurst() < b.getRemainingTimeInBurst());
+}
+
 void srt(vector<Process> p, int n){
 
     //Convert input process vector into a deque
     deque<Process> all_processes;
+    deque<Process> readyQ;
+    sort(readyQ.begin(), readyQ.end(), sortHelper);
     for(int i =0; i < p.size(); i++){
         all_processes.push_back(p[i]);
     }
