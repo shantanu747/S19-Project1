6b4b22bf4f5b76bf9ecf803f96a80db91a66be44
Daniel <schnolldj@gmail.com>
2019-03-18 17:20:20 -0400

    starting srt

--- SRT.cpp
+++ SRT.cpp
@@ -11,4 +11,10 @@ using namespace std;
       // process vector | num inputs
 void srt(vector<Process> p, int n){
 
+    //Convert input process vector into a deque
+    deque<Process> all_processes;
+    for(int i =0; i < p.size(); i++){
+        all_processes.push_back(p[i]);
+    }
+
 }
